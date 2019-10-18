9fdc6fb61e85f27481f109b23f18a1de937c547b
spampas <49267419+spampas@users.noreply.github.com>
2019-10-18 12:20:31 +0200

    Update Reservation.java

--- code/Reservation.java
+++ code/Reservation.java
@@ -1,33 +1,36 @@
+package application;
+
 import javafx.beans.property.*;
 
   public class Reservation {
       
       private final SimpleIntegerProperty idReservation;
-      private final SimpleIntegerProperty idUser;
+      private final SimpleIntegerProperty idClient;
       private final SimpleIntegerProperty idRestaurant;
       private final SimpleStringProperty date;
       private final SimpleIntegerProperty seats;
       private final SimpleStringProperty hour;
  
-      public Reservation(int u, int r, String d, int s, String h) {
-        idUser = new SimpleIntegerProperty(u);
-        idRestaurant = new SimpleIntegerProperty(r);
+      public Reservation(int reservation, int client, int resturant, String d , int s, String h) {
+    	idReservation = new SimpleIntegerProperty(reservation);
+        idClient = new SimpleIntegerProperty(client);
+        idRestaurant = new SimpleIntegerProperty(resturant);
         date = new SimpleStringProperty(d);
         seats = new SimpleIntegerProperty(s);
         hour = new SimpleStringProperty(h);
-        idReservation = new SimpleIntegerProperty();
+        
       }
       
       public int getReservation() {return idReservation.get();};
-      public int getUser() { return idUser.get(); }
+      public int getUser() { return idClient.get(); }
       public int getRestaurant() { return idRestaurant.get(); }
       public String getDate() { return date.get(); }
       public int getSeats() { return seats.get(); }
       public String getHour() { return hour.get(); }
       
-      public void setReservation(int r) {idReservation.set(r);};
-      public void setUser(int u) { idUser.set(u); }
-      public void setRestaurant(int r) { idRestaurant.set(r); }
+      public void setReservation(int reservation) {idReservation.set(reservation);};
+      public void setUser(int client) { idClient.set(client); }
+      public void setRestaurant(int resturant) { idRestaurant.set(resturant); }
       public void setDate(String d) { date.set(d); }
       public void setSeats(int s) { seats.set(s); }
       public void setHour(String h) { hour.set(h); }
