742408511cfde7fac2311c4d2432f5ee75278134
LCima <49267461+LCima@users.noreply.github.com>
2019-10-18 12:18:52 +0200

    Corrette classi bean

--- code/Restaurant.java
+++ code/Restaurant.java
@@ -1,18 +1,21 @@
 import javafx.beans.property.*;
 
   public class Restaurant {
-      private final SimpleIntegerProperty id;
+      private final SimpleIntegerProperty idRestaurant;
+      private final SimpleIntegerProperty idUser;
       private final SimpleStringProperty name;
       private final SimpleStringProperty type;
-      private final SimpleIntegerProperty seats;
+      private final SimpleIntegerProperty price;
       private final SimpleStringProperty city;
       private final SimpleStringProperty address;
       private final SimpleStringProperty description;
-      private final SimpleIntegerProperty price;
-      private final SimpleStringProperty openingAt;
-      private final SimpleStringProperty closingAt;
+      private final SimpleIntegerProperty seats;
+      private final SimpleStringProperty opening;
  
-      public Restaurant(String n, String t, int s, String c, String a, String d, int p, String oa, String ca) {
+      public Restaurant(int idR, int idU, String n, String t, int p, String c, String a, String d, int s, String oa) {
+        
+        idRestaurant = new SimpleIntegerProperty(idR);
+        idUser = new SimpleIntegerProperty(idU);
         name = new SimpleStringProperty(n);
         type = new SimpleStringProperty(t);
         seats = new SimpleIntegerProperty(s);
@@ -20,12 +23,11 @@ import javafx.beans.property.*;
         address = new SimpleStringProperty(a);
         description = new SimpleStringProperty(d);
         price = new SimpleIntegerProperty(p);
-        openingAt = new SimpleStringProperty(oa);
-        closingAt = new SimpleStringProperty(ca);
-        id = new SimpleIntegerProperty();
+        opening = new SimpleStringProperty(oa);
       }
       
-      public int getId() {return id.get(); }
+      public int getIdRestaurant() { return idRestaurant.get(); }
+      public int getIdUser() { return idUser.get(); }
       public String getName() { return name.get(); }
       public String getType() { return type.get(); }
       public int getSeats() { return seats.get(); }
@@ -33,10 +35,10 @@ import javafx.beans.property.*;
       public String getAddress() { return address.get(); }
       public String getDescription() { return description.get(); }
       public int getPrice() { return price.get(); }
-      public String getOpeningAt() { return openingAt.get(); }
-      public String getClosingAt() { return closingAt.get(); }
+      public String getOpening() { return opening.get(); }
       
-      public void setId(int i) {id.set(i);}
+      public void setIdRestaurant(int i) { idRestaurant.set(i); }
+      public void setIdUser(int i) { idUser.set(i); }
       public void setName(String n) { name.set(n); }
       public void setType(String t) { type.set(t); }
       public void setSeats(int s) { seats.set(s); }
@@ -44,7 +46,6 @@ import javafx.beans.property.*;
       public void setAddress(String a) { address.set(a); }
       public void setDescription(String d) { description.set(d); }
       public void setPrice(int p) { price.set(p); }
-      public void setOpeningAt(String oa) { openingAt.set(oa); }
-      public void setClosingAt(String ca) { closingAt.set(ca); }
+      public void setOpening(String oa) { opening.set(oa); }
       
  }
--- code/User.java
+++ code/User.java
@@ -2,25 +2,21 @@ import javafx.beans.property.*;
 
   public class User {
       
-	  private final SimpleIntegerProperty userId;
+      private final SimpleIntegerProperty userId;
       private final SimpleStringProperty username;
       private final SimpleStringProperty password;
-      private final SimpleStringProperty type;
  
-      public User(String u, String p, String r) {
+      public User(int i, String u, String p) {
+        userId = new SimpleIntegerProperty(i);
         username = new SimpleStringProperty(u);
         password = new SimpleStringProperty(p);
-        type = new SimpleStringProperty(r);
-        userId = new SimpleIntegerProperty();
       }
       
       public int getUserId() {return userId.get();}
       public String getUsername() { return username.get(); }
       public String getPassword() { return password.get(); }
-      public String getType() { return type.get(); }
       
       public void setUserId(int i) {userId.set(i); };
       public void setUsername(String u) { username.set(u); }
       public void setPassword(String p) { password.set(p); }
-      public void setType(String r) { type.set(r); }
  } 
