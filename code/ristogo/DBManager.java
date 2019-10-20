7a21eb7d36784348b4242d0729f222eea6b9c380
spampas <49267419+spampas@users.noreply.github.com>
2019-10-20 12:33:37 +0200

    aggiornato DBManager e database

--- code/ristogo/DBManager.java
+++ code/ristogo/DBManager.java
@@ -16,180 +16,218 @@ public class DBManager{
 	      }
 	    }
 	
-	 
+//login	 
 	public static int login(User us) {
             try {
-		PreparedStatement ps = connectionToDB.prepareStatement("SELECT IdUtente, COUNT(*) AS Numero " 
-                                                                + "FROM utente WHERE nome= ? AND password = ?;");                         
+            	PreparedStatement ps = connectionToDB.prepareStatement("SELECT IdUtente, COUNT(*) AS Numero " 
+		                                                                + "FROM utente WHERE nome= ? AND password = ?;");                         
                 ps.setString(1, us.getUsername());
                 ps.setString(2, us.getPassword());
                 ResultSet res = ps.executeQuery();
-		if(res.getInt("Numero")== 1) {
+				if(res.getInt("Numero")== 1) {
                     return res.getInt("IdUtente");
-                    }
-		else {
+                }
+				else {
                     return -1;
-		}
-			 
-		}catch(SQLException e) {
-                    System.out.println(e.getMessage());
-                    return -1;
-		}
-		 
+				}			 
+            }catch(SQLException e) {
+            	System.out.println(e.getMessage());
+            	return -1;
+            }	 
 	 }
-	 
+	
+//register	 
 	public static int register(User us) {
 		
             int res = -1;
             try {
-		PreparedStatement ps = connectionToDB.prepareStatement("INSERT INTO utente (nome, password) " 
-										+ "VALUES(?,?)");
-                ps.setString(1, us.getUsername());
-                ps.setString(2, us.getPassword());
-		res = ps.executeUpdate();
-                         
-            }catch(SQLException e) {
-                System.out.println(e.getMessage());
+				PreparedStatement ps = connectionToDB.prepareStatement("INSERT INTO utente (nome, password) " 
+												+ "VALUES(?,?)");
+            	ps.setString(1, us.getUsername());
+	            ps.setString(2, us.getPassword());
+				res = ps.executeUpdate();
+				
+				int id;
+				PreparedStatement ps1 = connectionToDB.prepareStatement("SELECT IdUtente"
+																		+"FROM utente WHERE nome= ? AND password = ?;");
+                ps1.setString(1, us.getUsername());
+                ps1.setString(2, us.getPassword());
+                ResultSet result = ps1.executeQuery();
+                id = result.getInt("IdUtente");
+				
+				PreparedStatement ps2 = connectionToDB.prepareStatement("INSERT INTO ristorante (IdUtente, nome, genere, costo, citta, via, descrizione, coperti, apertura)"
+																		+ "VALUES(?, null, null, null, null, null, null, null, null)");
+				ps2.setInt(1, id);
+			                     
+	        }catch(SQLException e) {
+			            System.out.println(e.getMessage());
             }
             return res;
 	}
 	 
-	 
+//list of restaurant	 
 	public static List<Restaurant> list_restaurant(){
 		 
             List<Restaurant> restaurants = new ArrayList<>();
             try {
                 PreparedStatement ps = connectionToDB.prepareStatement("SELECT* " 
                                                                     + "FROM ristorante;");
-		ResultSet res = ps.executeQuery();
-		while(res.next()) {
-                    int idRestaurant = res.getInt("IdRisto");
-                    int idUser = res.getInt("IdUtente");
-                    String name = res.getString("nome");
-                    String type = res.getString("genere");
-                    int price = res.getInt("costo");				 
-                    String city = res.getString("citta");
-                    String address = res.getString("via");
-                    String description = res.getString("descrizione");
-                    int seats = res.getInt("coperti");
-                    String open = res.getString("apertura");
+                ResultSet res = ps.executeQuery();
+				while(res.next()) {
+	                int idRestaurant = res.getInt("IdRisto");
+	                int idUser = res.getInt("IdUtente");
+	                String name = res.getString("nome");
+	                String type = res.getString("genere");
+	                int price = res.getInt("costo");				 
+	                String city = res.getString("citta");
+	                String address = res.getString("via");
+	                String description = res.getString("descrizione");
+	                int seats = res.getInt("coperti");
+	                String open = res.getString("apertura");
 				 
-                    restaurants.add(new Restaurant(idRestaurant, idUser, name, type, price, city, address, description, seats, open));
-				 
-		}
+	                restaurants.add(new Restaurant(idRestaurant, idUser, name, type, price, city, address, description, seats, open));
+						 
+				}
 			 
             }catch(SQLException e) {
-		System.out.println(e.getMessage());
+            	System.out.println(e.getMessage());
             }
-		  
             return restaurants;
 	 }
 	
+//modify a restaurant
+	public static int updateRestaurant(Restaurant r) {
+                 
+        int res = -1;
+        try {
+        	PreparedStatement ps = connectionToDB.prepareStatement("UPDATE ristorante " +
+                    "SET IdUtente = ? AND nome = ? AND genere = ? AND costo = ? AND citta = ? AND via = ? " +
+                    "AND descrizione = ? AND coperti = ? AND apertura = ? WHERE IdRisto = ? "); 
+            ps.setInt(1, r.getIdUser());
+            ps.setString(2, r.getName());
+            ps.setString(3,r.getType());
+            ps.setInt(4, r.getPrice());
+            ps.setString(5, r.getCity());
+            ps.setString(6, r.getAddress());
+            ps.setString(7, r.getDescription());
+            ps.setInt(8, r.getSeats());
+            ps.setString(9, r.getOpening());
+            ps.setInt(10, r.getIdRestaurant());                         
+            res = ps.executeUpdate();
+		
+        }catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }	 
+        return res;
+	}
+	
+//check for a table
+	public static int check(Reservation r) {
+       
+		
+        try {
+            PreparedStatement ps = connectionToDB.prepareStatement("SELECT (r.coperti - SUM(p.coperti)) AS PostiRimasti"
+            														+"FROM ristorante r INNER JOIN prenotazione p ON r.IdRisto = p.IdRisto"
+            														+ " WHERE p.IdRisto= ? AND p.data = ? AND p.orario=? ;");
+						
+			ps.setInt(1, r.getRestaurant());
+			ps.setString(3,r.getDate());
+			ps.setString(4,r.getHour());
+			ResultSet res = ps.executeQuery();
+			return res.getInt("PostiRimasti");
+
+        }catch(SQLException e) {
+        	System.out.println(e.getMessage());
+        	return -1;
+        }
+        
+ }
+	
+
+//book a table
 	public static int book(Reservation r) {
                  
             int res = -1;
             try {
                 PreparedStatement ps = connectionToDB.prepareStatement("INSERT INTO prenotazione " 
 							+ "(IdCliente, IdRisto, data, orario, persone) VALUES(?,?,?,?,?);");
-		ps.setInt(1, r.getUser());
-		ps.setInt(2,r.getRestaurant());
-		ps.setString(3,r.getDate());
-		ps.setString(4,r.getHour());
-		ps.setInt(5, r.getSeats());
-		res = ps.executeUpdate();
+				ps.setInt(1, r.getUser());
+				ps.setInt(2,r.getRestaurant());
+				ps.setString(3,r.getDate());
+				ps.setString(4,r.getHour());
+				ps.setInt(5, r.getSeats());
+				res = ps.executeUpdate();
 
             }catch(SQLException e) {
-		System.out.println(e.getMessage());
+            	System.out.println(e.getMessage());
             }	 
             return res;
 	 }
+
+//list of reservation (client/restauranteur) 
 	
-	public static List<Reservation> list_reservation_client(User s){
+	MANCA LA PARTE RISTORATORE
+	public static List<Reservation> list_reservation(User s){
 	
             List<Reservation> reservations = new ArrayList<>();
             try {
-		PreparedStatement ps = connectionToDB.prepareStatement("SELECT p.IdPrenotazione AS Prenotazione "
+            	PreparedStatement ps = connectionToDB.prepareStatement("SELECT p.IdPrenotazione AS Prenotazione "
                 + "r.IdUtente AS Utente, p.data As Data, r.IdRisto AS Ristorante, p.orario AS Orario, p.persone AS Persone "  
-		+ "FROM prenotazione p INNER JOIN ristorante r ON p.IdRisto = r.IdRisto "
+                + "FROM prenotazione p INNER JOIN ristorante r ON p.IdRisto = r.IdRisto "
                 + "WHERE idCliente = ?;");
                 
                 ps.setInt(1, s.getUserId());
                 ResultSet res = ps.executeQuery();
-		while(res.next()) {
-                    int reservation = res.getInt("Prenotazione");
-                    int user = res.getInt("Utente");
-                    int restaurant = res.getInt("Ristorante");
-                    String date = res.getString("Data");
-                    String hour = res.getString("Orario");
-                    int seats = res.getInt("Persone");
+				while(res.next()) {
+	                int reservation = res.getInt("Prenotazione");
+	                int user = res.getInt("Utente");
+	                int restaurant = res.getInt("Ristorante");
+	                String date = res.getString("Data");
+	                String hour = res.getString("Orario");
+	                int seats = res.getInt("Persone");
 				 
-                    reservations.add(new Reservation(reservation, user, restaurant, date, hour, seats));
+	                reservations.add(new Reservation(reservation, user, restaurant, date, hour, seats));
 				 
-		}
-			 
+				}
             }catch(SQLException e) {
                 System.out.println(e.getMessage());
             }
 		  
             return reservations;   
 	}
-         
-        public static int deleteReservation(Reservation r) {
-        
-            int res = -1;
-            try {
-		PreparedStatement ps = connectionToDB.prepareStatement("DELETE * FROM prenotazione " + 
-                                                                       "WHERE IdPrenotazione = ?"); 									
-		ps.setInt(1, r.getReservation());
-		res = ps.executeUpdate();
-                
-            }catch(SQLException e) {
-		System.out.println(e.getMessage());
-            }	 
-            return res;
-	}
-         
-        public static int updateReservation(Reservation r) {
-                 
-            int res = -1;
-            try {
-                PreparedStatement ps = connectionToDB.prepareStatement("UPDATE prenotazione " + 
-                                                    "SET data = ? AND orario = ? and persone = ? WHERE IdPrenotazione = ? "); 
-                ps.setString(1, r.getDate());
-                ps.setString(2,r.getHour());
-                ps.setInt(3, r.getSeats());
-                ps.setInt(4, r.getReservation());	 
-                res = ps.executeUpdate();
 
-            }catch(SQLException e) {
-		System.out.println(e.getMessage());
-            }	 
-            return res;
-	}
+//delete a reservation(Not used)
+	public static int deleteReservation(Reservation r) {
 	
-	public static int updateRestaurant(Restaurant r) {
-                 
-            int res = -1;
-            try {
-		PreparedStatement ps = connectionToDB.prepareStatement("UPDATE ristorante " +
-                        "SET IdUtente = ? AND nome = ? AND genere = ? AND costo = ? AND citta = ? AND via = ? " +
-                        "AND descrizione = ? AND coperti = ? AND apertura = ? WHERE IdRisto = ? "); 
-                ps.setInt(1, r.getIdUser());
-                ps.setString(2, r.getName());
-                ps.setString(3,r.getType());
-                ps.setInt(4, r.getPrice());
-                ps.setString(5, r.getCity());
-                ps.setString(6, r.getAddress());
-                ps.setString(7, r.getDescription());
-                ps.setInt(8, r.getSeats());
-                ps.setString(9, r.getOpening());
-                ps.setInt(10, r.getIdRestaurant());                         
-                res = ps.executeUpdate();
-			
-            }catch(SQLException e) {
-                System.out.println(e.getMessage());
-            }	 
-            return res;
+	    int res = -1;
+	    try {
+			PreparedStatement ps = connectionToDB.prepareStatement("DELETE * FROM prenotazione " + 
+			                                                               "WHERE IdPrenotazione = ?"); 									
+			ps.setInt(1, r.getReservation());
+			res = ps.executeUpdate();
+	            
+        }catch(SQLException e) {
+        	System.out.println(e.getMessage());
+	        }	 
+        return res;
+	}
+//modify a reservation(not used)         
+	public static int updateReservation(Reservation r) {
+	         
+	    int res = -1;
+	    try {
+	        PreparedStatement ps = connectionToDB.prepareStatement("UPDATE prenotazione " + 
+	                                            "SET data = ? AND orario = ? and persone = ? WHERE IdPrenotazione = ? "); 
+            ps.setString(1, r.getDate());
+            ps.setString(2,r.getHour());
+            ps.setInt(3, r.getSeats());
+            ps.setInt(4, r.getReservation());	 
+            res = ps.executeUpdate();
+	
+        }catch(SQLException e) {
+        	System.out.println(e.getMessage());
+	        }	 
+        return res;
 	}
 	
 	
